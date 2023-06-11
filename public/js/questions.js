22b6d7440954e275f3d79cf0c92d3af83f67d1b8
Adonias Fernandes Falcão <adoniasfalcao036@gmail.com>
2023-06-11 08:46:29 -0300

    Arquivos

---
+++ public/js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+  numb: 1,
+  question: "Qual figura de linguagem é utilizada para dar outro sentido para uma palavra ou expressão?",
+  answer: "Metáfora",
+  options: [
+    "Ironia",
+    "Anáfora",
+    "Metonímia",
+    "Metáfora"
+  ]
+},
+  {
+  numb: 2,
+  question: "Na frase 'O luar será o leito de morte e renascimento' há a presença de duas figuras de linguagem. Quais são elas?",
+  answer: "Metáfora e antítese",
+  options: [
+    "Metáfora e personificação",
+    "Metonímia e antítese",
+    "Metáfora e antítese",
+    "Hipérbole e ironia"
+  ]
+},
+  {
+  numb: 3,
+  question: "A primeira fase do modernismo literário brasileiro, que durou de 1922 a 1930, teve como principal característica:",
+  answer: "Valorização das raízes culturais brasileiras.",
+  options: [
+    "Uso de poemas de forma fixa, como o soneto.",
+    "Valorização das raízes culturais brasileiras.",
+    "Foco em temas relacionados com a colonização",
+    "Linguagem rebuscada e acadêmica"
+  ]
+},
+  {
+  numb: 4,
+  question: "A poesia brasileira do Romantismo do século XIX pode ser dividida em:",
+  answer: "Três fases: 1º indianista, 2º subjetiva, 3º liberal e social.",
+  options: [
+    "Quatro fases: 1º histórica, 2º crítica nacionalista, 3º experimental e 4º subjetiva",
+    "Três fases: 1º subjetiva, 2º nacionalista e 3º experimental.",
+    "Duas fases: 1º sentimental e 2º fase nacionalista.",
+    "Três fases: 1º indianista, 2º subjetiva, 3º liberal e social."
+  ]
+},
+  {
+  numb: 5,
+  question: " 'A água do mar é azul', 'A água do rio é fria' De acordo com o trecho acima, qual a figura de linguagem empregada?",
+  answer: "Anáfora",
+  options: [
+    "Hipérbato",
+    "Anáfora",
+    "Assíndeto",
+    "Polissíndeto"
+  ]
+},
+// you can uncomment the below codes and make duplicate as more as you want to add question
+// but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+//   {
+//   numb: 6,
+//   question: "Your Question is Here",
+//   answer: "Correct answer of the question is here",
+//   options: [
+//     "Option 1",
+//     "option 2",
+//     "option 3",
+//     "option 4"
+//   ]
+// },
+];
